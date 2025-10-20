29c63c2480e26a100b71cc9c6500859101963a2a
Mukhammad <m.makhmudoff@gmail.com>
2025-10-20 14:21:02 +0300

    Add Xcode project files

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Мухаммад Махмудов on 16.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
